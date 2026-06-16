80391eb65ffa0f9bca9e46a1d1851e730cad0876
nohamoulma-hub <noham.oulma@gmail.com>
2026-06-16 16:26:38 +0200

    feat: add async_generator function to yield random floats asynchronously

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Module called async_generator that takes no arguments """
+
+import asyncio
+import random
+from typing import AsyncGenerator, List
+
+async def async_generator() -> AsyncGenerator[float, None]:
+    """The coroutine will loop 10 times, each time asynchronously wait 1 second """
+    for i in range(10):
+        await asyncio.sleep(1)  
+        yield random.uniform(0, 10)
