cae2d5d9ed2f049ea9431068d0c6b25fb93f2035
nohamoulma-hub <noham.oulma@gmail.com>
2026-06-16 16:28:25 +0200

    fix: update docstring formatting and improve variable naming in async_generator

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -3,10 +3,13 @@
 
 import asyncio
 import random
-from typing import AsyncGenerator, List
+from typing import AsyncGenerator
+
 
 async def async_generator() -> AsyncGenerator[float, None]:
-    """The coroutine will loop 10 times, each time asynchronously wait 1 second """
-    for i in range(10):
-        await asyncio.sleep(1)  
+    """The coroutine will loop 10 times,
+    each time asynchronously wait 1 second """
+
+    for _ in range(10):
+        await asyncio.sleep(1)
         yield random.uniform(0, 10)
