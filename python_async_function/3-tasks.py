02ed93c21e25181605e24b127d6a4bcab38a5e80
nohamoulma-hub <noham.oulma@gmail.com>
2026-06-16 15:57:01 +0200

    fix: update docstring in task_wait_random for clarity

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -6,6 +6,7 @@ import asyncio
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
+
 def task_wait_random(max_delay: int) -> asyncio.Task:
-    """ """
+    """ that takes an integer max_delay and returns a asyncio.Task. """
     return asyncio.create_task(wait_random(max_delay))
