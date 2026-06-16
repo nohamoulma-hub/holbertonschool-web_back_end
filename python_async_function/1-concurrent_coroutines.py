92d0a1885d11b2104753f9c48a17d2bb91a02f8f
nohamoulma-hub <noham.oulma@gmail.com>
2026-06-16 13:15:51 +0200

    fix: set default value for max_delay parameter in wait_n function

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -7,7 +7,7 @@ from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> List[float]:
+async def wait_n(n: int, max_delay: int = 10) -> List[float]:
     # Liste qui va stocker les délais, dans l'ordre où ils se terminent
     delays = []
     # On crée n coroutines wait_random(max_delay) d'un coup
