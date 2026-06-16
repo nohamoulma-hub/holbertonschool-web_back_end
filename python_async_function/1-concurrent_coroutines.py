7d55205517fcef3aa6c72e574dd37a91694727b8
nohamoulma-hub <noham.oulma@gmail.com>
2026-06-16 13:31:26 +0200

    fix: remove default value for max_delay parameter in wait_n function

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -7,15 +7,15 @@ from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int = 10) -> List[float]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """ print the list of the delays should be in ascending
     order without using sort() because of concurrency."""
 
-    # Liste qui va stocker les délais, dans l'ordre où ils se terminent
-    delays = []
-
     # On crée n coroutines wait_random(max_delay) d'un coup
     tasks = [wait_random(max_delay) for _ in range(n)]
+
+    # Liste qui va stocker les délais, dans l'ordre où ils se terminent
+    delays = []
 
     # asyncio.as_completed planifie toutes les tasks pour qu'elles
     # s'exécutent en concurrence, puis elle les redonne une par une,
