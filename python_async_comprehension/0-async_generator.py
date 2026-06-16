73d3e254d5793fed44d793fddf457ebc0cc23014
nohamoulma-hub <noham.oulma@gmail.com>
2026-06-16 16:34:34 +0200

    fix: update return type annotation in async_generator to use Generator

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -3,10 +3,10 @@
 
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """The coroutine will loop 10 times,
     each time asynchronously wait 1 second """
 
